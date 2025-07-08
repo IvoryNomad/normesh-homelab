aabb408ae8f609d757263439f07ba87d6fc92f93
Nathan Eric Norman <nenorman@norme.sh>
2025-07-08 15:15:05 +0000

    Pull in secrets

---
+++ tf/main.tf
@@ -0,0 +1,16 @@
+# main.tf
+data "external" "deb_template_creds" {
+  program = ["bash", "-c", "op_wrapper item get deb-template-creds --vault CICD-homelab --fields username,password --format json | jq 'reduce .[] as $item ({}; .[$item.label] = $item.value)'"]
+}
+
+data "external" "minio_admin_creds" {
+  program = ["bash", "-c", "op_wrapper item get minio-admin --vault CICD-homelab --fields username,password --format json | jq 'reduce .[] as $item ({}; .[$item.label] = $item.value)'"]
+}
+
+data "external" "proxmox_admin_creds" {
+  program = ["bash", "-c", "op_wrapper item get proxmox-root --vault CICD-homelab --fields username,password --format json | jq 'reduce .[] as $item ({}; .[$item.label] = $item.value)'"]
+}
+
+data "external" "tofu_idm_api_creds" {
+  program = ["bash", "-c", "op_wrapper item get tofu-idm-api --vault CICD-homelab --fields username,password --format json | jq 'reduce .[] as $item ({}; .[$item.label] = $item.value)'"]
+}
