5d7624063d13f710cd3a8e1135e2cb49ed236df2
Nathan Eric Norman <nenorman@norme.sh>
2025-07-08 20:55:30 +0000

    Create tool to clean pve cloud-init templates
    need to fix install dirs to prefer .local/bin

---
+++ tools/template_clean/template_clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_DIR="/usr/local/share"
+SCRIPT_NAME="template_clean"
+
+if [ -n "${SUDO_USER}" ]; then
+  echo "WARNING: Running with sudo prevents overwriting ${SUDO_USER}'s bash history file" 1>&2
+  read -p "Continue? [y/N]" response
+  if [[ ! "$response" =~ ^[yY]$ ]]; then
+    echo "Aborting."
+    exit 1
+  fi
+  HISTFILE=/dev/null /bin/bash "${SCRIPT_DIR}/${SCRIPT_NAME}" "$@"
+else
+  history -w ~/.bash_history
+  sudo HISTFILE=/dev/null /bin/bash "${SCRIPT_DIR}/${SCRIPT_NAME}" "$@"
+fi
