3e4c531d11d9cee27d62053c546c88a757215c87
Nathan Eric Norman <nenorman@norme.sh>
2025-07-13 14:57:34 -0400

    Use AWS S3 bucket

--- tf/backend.tf
+++ tf/backend.tf
@@ -1,13 +1,10 @@
 # backend.tf
 terraform {
   backend "s3" {
-    endpoint                    = "http://minio.idm.norme.sh:9000" # Use hostname assuming DNS
-    bucket                      = "terraform-state"
-    key                         = "terraform.tfstate"
-    region                      = "us-east-1"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    use_path_style              = true
+    bucket         = "normesh-homelab-tofu-state"
+    key            = "normesh-homelab/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tofu-state-lock"
+    encrypt        = true
   }
 }
