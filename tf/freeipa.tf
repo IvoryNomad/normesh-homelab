829f68d9a92a4a634ae81d407f5cf18c033693a2
Nathan Eric Norman <nenorman@norme.sh>
2025-07-19 22:39:11 -0400

    Abstract hcl to use vars files

--- tf/freeipa.tf
+++ tf/freeipa.tf
@@ -1,241 +1,72 @@
-# incus.tf
+# freeipa.tf
 
-resource "incus_instance" "ipa1" {
-  name        = "ipa1"
-  image       = "alma-9-cloud-tmpl"
+resource "incus_instance" "ipa_servers" {
+  for_each    = local.ipa_servers
+
+  name        = each.key
+  # image       = "alma-9-cloud-tmpl"
+  image       = "images:almalinux/9/cloud"
   type        = "container"
-  remote      = "vmhost0"
-  description = "ipa1.ipa.norme.sh, managed by opentofu"
+  remote      = "cluster0"
+  description = "${each.value.fqdn}, managed by opentofu"
 
   config = {
-    "cloud-init.user-data" = <<-EOT
-      #cloud-config
-      hostname: ipa1
-      fqdn: ipa1.ipa.norme.sh
+    "cloud-init.user-data" = templatefile("cloud-init-user-data.yml.tpl", {
+      bootcmds            = each.value.bootcmds
+      hostname            = each.value.hostname
+      fqdn                = each.value.fqdn
+      local_users         = each.value.local_users
+      ssh_keys            = local.ssh_keys
+      additional_packages = each.value.additional_packages
+      runcmds             = each.value.runcmds
+    })
 
-      users:
-        - name: localadmin
-          groups: adm
-          lock_passwd: false
-          sudo: ALL=(ALL) NOPASSWD:ALL
-          ssh_authorized_keys:
-            - "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIh3xF5/PwXtFQexMzoEbKI0dFE/Ddu2CdD+Y9OJX5HW ansible@normesh"
-            - "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIzfTYaJ7JmPK5ZqPVDvFAfsnwXb8wtJj+FzFbMCuLpV nenorman@normesh"
-    EOT
-
-    "cloud-init.network-config" = <<-EOT
-      version: 1
-      config:
-        - type: physical
-          name: eth0
-          subnets:
-          - type: static
-            address: '192.168.226.2/24'
-            gateway: '192.168.226.1'
-          - type: static6
-            address: '2001:470:e0fc:2::2/64'
-            gateway: '2001:470:e0fc:2::1'
-        - type: nameserver
-          address:
-          - '192.168.225.6'
-          - '192.168.225.7'
-          search:
-          - 'ipa.norme.sh'
-    EOT
+    "cloud-init.network-config" = templatefile("cloud-init-network-config.yml.tpl", {
+      ifname          = each.value.ifname
+      ipv4_addr       = each.value.ipv4_addr
+      ipv4_mask       = each.value.ipv4_mask
+      ipv4_gw         = each.value.ipv4_gw
+      ipv6_addr       = each.value.ipv6_addr
+      ipv6_mask       = each.value.ipv6_mask
+      ipv6_gw         = each.value.ipv6_gw
+      dns_servers     = local.dns_servers
+      search_domains  = local.search_domains
+    })
   }
 
-  profiles = ["storage", "net-vmbr21", "freeipa-server"]
+  profiles = ["default", "freeipa-server"]
 
   running = true
 
-  wait_for {
-    type  = "delay"
-    delay = "30s"
-  }
-
   provisioner "local-exec" {
-    command = "cd ../ansible && ansible-playbook -i inventory.yml 00-playbook-ssh-keys.yml --limit ipa1.ipa.norme.sh"
-  }
+    command = <<-EOT
+      echo "Waiting for cloud-init to complete..."
 
-  provisioner "local-exec" {
-    command = "cd ../ansible && ansible-playbook -i inventory.yml freeipa-server.yml --tags install,reboot,backup --limit ipa1.ipa.norme.sh"
-  }
-
-  provisioner "local-exec" {
-    command = "incus snapshot create vmhost0:ipa1 first-master-configured"
-  }
-}
-
-output "ipa1_ipv4" {
-  value       = incus_instance.ipa1.ipv4_address
-  description = "IPv4 address of ipa1"
-}
-
-output "ipa1_status" {
-  value       = incus_instance.ipa1.status
-  description = "Status of ipa1"
-}
-
-
-resource "incus_instance" "ipa2" {
-  name        = "ipa2"
-  image       = "alma-9-cloud-tmpl"
-  type        = "container"
-  remote      = "vmhost0"
-  description = "ipa2.ipa.norme.sh, managed by opentofu"
-
-  # ensure first master exists
-  depends_on = [
-    incus_instance.ipa1
-  ]
-
-  config = {
-    "cloud-init.user-data" = <<-EOT
-      #cloud-config
-      hostname: ipa2
-      fqdn: ipa2.ipa.norme.sh
-
-      users:
-        - name: localadmin
-          groups: adm
-          lock_passwd: false
-          sudo: ALL=(ALL) NOPASSWD:ALL
-          ssh_authorized_keys:
-            - "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIh3xF5/PwXtFQexMzoEbKI0dFE/Ddu2CdD+Y9OJX5HW ansible@normesh"
-            - "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIzfTYaJ7JmPK5ZqPVDvFAfsnwXb8wtJj+FzFbMCuLpV nenorman@normesh"
-    EOT
-
-    "cloud-init.network-config" = <<-EOT
-      version: 1
-      config:
-        - type: physical
-          name: eth0
-          subnets:
-          - type: static
-            address: '192.168.226.3/24'
-            gateway: '192.168.226.1'
-          - type: static6
-            address: '2001:470:e0fc:2::3/64'
-            gateway: '2001:470:e0fc:2::1'
-        - type: nameserver
-          address:
-          - '192.168.225.6'
-          - '192.168.225.7'
-          search:
-          - 'ipa.norme.sh'
+      incus exec cluster0:${each.key} -- cloud-init status --wait >/dev/null 2>&1
+      RC=$?
+      if [ $RC -eq 0 ]; then
+        echo "Cloud-init completed successfully"
+      elif [ $RC -eq 2 ]; then
+        echo "Cloud-init had recoverable errors"
+        incus exec cluster0:${each.key} -- cloud-init status --long
+        exit 0
+      else
+        echo "Cloud-init failed!"
+        incus exec cluster0:${each.key} -- cloud-init status --long
+        exit 1
+      fi
     EOT
   }
 
-  profiles = ["storage", "net-vmbr21", "freeipa-server"]
-
-  running = true
-
-  wait_for {
-    type  = "delay"
-    delay = "30s"
+  provisioner "local-exec" {
+    command = "cd ../ansible && ansible-playbook -i inventory.yml 00-playbook-ssh-keys.yml --limit ${each.value.fqdn}"
   }
 
   provisioner "local-exec" {
-    command = "cd ../ansible && ansible-playbook -i inventory.yml 00-playbook-ssh-keys.yml --limit ipa2.ipa.norme.sh"
+    command = "cd ../ansible && ansible-playbook -i inventory.yml freeipa-server.yml --tags install,backup --limit ${each.value.fqdn}"
   }
 
   provisioner "local-exec" {
-    command = "cd ../ansible && ansible-playbook -i inventory.yml freeipa-server.yml --tags install,reboot --limit ipa2.ipa.norme.sh"
+    command = "incus snapshot create cluster0:${each.key} ${each.value.snapshot_name}"
   }
-
-  provisioner "local-exec" {
-    command = "incus snapshot create vmhost0:ipa2 replica-configured"
-  }
-}
-
-output "ipa2_ipv4" {
-  value       = incus_instance.ipa2.ipv4_address
-  description = "IPv4 address of ipa2"
-}
-
-output "ipa2_status" {
-  value       = incus_instance.ipa2.status
-  description = "Status of ipa2"
-}
-
-resource "incus_instance" "ipa3" {
-  name        = "ipa3"
-  image       = "alma-9-cloud-tmpl"
-  type        = "container"
-  remote      = "vmhost0"
-  description = "ipa3.ipa.norme.sh, managed by opentofu"
-
-  # ensure first master exists
-  depends_on = [
-    incus_instance.ipa1,
-    incus_instance.ipa2
-  ]
-
-  config = {
-    "cloud-init.user-data" = <<-EOT
-      #cloud-config
-      hostname: ipa3
-      fqdn: ipa3.ipa.norme.sh
-
-      users:
-        - name: localadmin
-          groups: adm
-          lock_passwd: false
-          sudo: ALL=(ALL) NOPASSWD:ALL
-          ssh_authorized_keys:
-            - "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIh3xF5/PwXtFQexMzoEbKI0dFE/Ddu2CdD+Y9OJX5HW ansible@normesh"
-            - "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIzfTYaJ7JmPK5ZqPVDvFAfsnwXb8wtJj+FzFbMCuLpV nenorman@normesh"
-    EOT
-
-    "cloud-init.network-config" = <<-EOT
-      version: 1
-      config:
-        - type: physical
-          name: eth0
-          subnets:
-          - type: static
-            address: '192.168.226.4/24'
-            gateway: '192.168.226.1'
-          - type: static6
-            address: '2001:470:e0fc:2::4/64'
-            gateway: '2001:470:e0fc:2::1'
-        - type: nameserver
-          address:
-          - '192.168.225.6'
-          - '192.168.225.7'
-          search:
-          - 'ipa.norme.sh'
-    EOT
-  }
-
-  profiles = ["storage", "net-vmbr21", "freeipa-server"]
-
-  running = true
-
-  wait_for {
-    type  = "delay"
-    delay = "30s"
-  }
-
-  provisioner "local-exec" {
-    command = "cd ../ansible && ansible-playbook -i inventory.yml 00-playbook-ssh-keys.yml --limit ipa3.ipa.norme.sh"
-  }
-
-  provisioner "local-exec" {
-    command = "cd ../ansible && ansible-playbook -i inventory.yml freeipa-server.yml --tags install,reboot --limit ipa3.ipa.norme.sh"
-  }
-
-  provisioner "local-exec" {
-    command = "incus snapshot create vmhost0:ipa3 replica-configured"
-  }
-}
-
-output "ipa3_ipv4" {
-  value       = incus_instance.ipa3.ipv4_address
-  description = "IPv4 address of ipa3"
-}
-
-output "ipa3_status" {
-  value       = incus_instance.ipa3.status
-  description = "Status of ipa3"
 }
